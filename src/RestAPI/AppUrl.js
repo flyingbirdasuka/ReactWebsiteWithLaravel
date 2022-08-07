7fefaed762fde5b079d2cf7c87f8de5190ff448b
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2022-08-07 14:32:07 +0200

    Change port number of Laravel project back

--- src/RestAPI/AppUrl.js
+++ src/RestAPI/AppUrl.js
@@ -1,5 +1,5 @@
 class AppUrl {
-    static BaseURL = 'http://admin.asuka-developer.com:8080/api';
+    static BaseURL = 'http://admin.asuka-developer.com/api';
 
     static HomeTopTile = this.BaseURL + '/homepage/title';
     static HomeTechDesc = this.BaseURL + '/techhome';
